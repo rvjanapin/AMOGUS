08cf36d5517262f47dc78b6880ace4ac5bbb6daa
vitto <vittojanapin@outlook.com>
2022-02-17 09:03:49 +0800

    added sound

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const music = new Audio('report.mp3')
+
 function SUS(){
-    crewmate.src = "deadbody.png"
+    crewmate.src = "deadbody.png";
+    music.play();
+    document.getElementById("red").innerHTML = "YOU SUS"
+    setTimeout(() => {
+        crewmate.src = "AMOGUS.png";
+        console.log("change")
+        document.getElementById("red").innerHTML = ""
+    }, 3000);
 }
